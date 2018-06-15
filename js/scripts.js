315fe498322765f60a00b3cccda98442bc8bc010
Guest User <Guest@Epicodus-7E.local>
2018-06-15 13:48:01 -0700

    pizza size items prices and a function to check if there is an item in object list

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,44 @@
-function Pizza(name,size){
+function PizzaOrder(name,size){
   this.name = name;
   this.size = size;
   // this.sauce = sauce;
   // this.toppings = toppings
 }
 
-Pizza.prototype.addCustomSauce = function () {
+PizzaOrder.prototype.addCustomSauce = function () {
   this.customSauce = "customSauce"
 
 };
 
+PizzaOrder.prototype.addCustomToppings = function () {
+  this.customSauce = "customSauce"
+
+};
+
+PizzaOrder.prototype.addCustomStyle = function () {
+  this.customSauce = "customSauce"
+
+};
+
+var itemsCost = [["personal",5],["small",7],["medium",10],["large",14],["Xlarge",21]]
 
 
+
+
+// Pizza.porototype.itemsCost = function () {
+//   this.itemsCost = {small:
+//
+//   }
+//
+// }
+//
+PizzaOrder.prototype.getPizzaPrice = function (item){
+  if (item === true){
+    alert("yes")
+  }
+
+
+}
 
 
 
@@ -26,17 +53,21 @@ $(document).ready(function() {
     var inputtedName = $("#inputName").val();
     var inputtedSize= $("#pizzaSize").val();
     var inputtedSauce = $("#pizzaSauce").val();
-    console.log(inputtedSize)
+    var inputtedToppings = $("#pizzaToppings").val();
+    var inputtedStyle = $("#pizzaStyle").val();
 
-    var pizzaOrder = new Pizza(inputtedName,inputtedSize)
-    pizzaOrder.addCustomSauce(inputtedSauce)
+    var pizza = new PizzaOrder(inputtedName,inputtedSize)
+    pizza.addCustomSauce(inputtedSauce)
+    console.log(typeof(pizza.size))
+    console.log(pizza.hasOwnProperty("size"))
+    pizza.getPizzaPrice(pizza.hasOwnProperty("size"))
 
-    console.log("sauce" + inputtedSauce)
+
+    console.log(inputtedName + inputtedSize + inputtedSauce + inputtedToppings + inputtedStyle)
 
 
     $("#receipt").show();
-    $(".custom").append("<li class='custom'>" + pizzaOrder.size + "</l1>")
+    $(".custom").append("<li class='custom'>" + pizza.size + "</l1>")
 
   });
-
 });
